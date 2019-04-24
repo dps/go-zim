2e5ec5029d35a646ecc6a6ec5845de5c6b2b612d
Tim <tim-st@users.noreply.github.com>
2019-04-24 14:16:47 +0200

    Update zimtext.go

--- cmd/zimtext/zimtext.go
+++ cmd/zimtext/zimtext.go
@@ -19,9 +19,9 @@ func main() {
 	var singleSentences bool
 
 	flag.StringVar(&filenameZim, "zim", "", "Path to the ZIM file to read from.")
-	flag.StringVar(&filenameText, "txt", "", "Path to the target text file (truncated if exists).")
-	flag.IntVar(&limit, "limit", -1, "Stop after `limit` lines were written.")
-	flag.BoolVar(&singleSentences, "sentences", false, "Only take paragraphs which are likely a single sentence.")
+	flag.StringVar(&filenameText, "txt", "", "Path to the text file, that is created or truncated if exists.")
+	flag.IntVar(&limit, "limit", -1, "Stop after N lines were written (where N >= limit).")
+	flag.BoolVar(&singleSentences, "sentences", false, "Only write paragraphs which are likely a single sentence.")
 	flag.Parse()
 
 	if flag.NFlag() < 2 || len(filenameZim) == 0 || len(filenameText) == 0 {
@@ -41,7 +41,7 @@ func main() {
 		log.Fatal(txtFileErr)
 	}
 
-	var bufWriter = bufio.NewWriter(txtFile)
+	var bufWriter = bufio.NewWriterSize(txtFile, 1<<22) // 4mb buffer
 
 	var paragraphsWritten = 0
 
@@ -64,6 +64,12 @@ func main() {
 		}
 	}
 
+	defer func() {
+		fmt.Print("\r100.0%")
+		bufWriter.Flush()
+		txtFile.Close()
+	}()
+
 	for clusterPosition := uint32(0); clusterPosition < z.ClusterCount(); clusterPosition++ {
 
 		printProgress(clusterPosition)
@@ -80,19 +86,16 @@ func main() {
 
 		for blobPosition := uint32(0); ; blobPosition++ {
 
+			var requiredParagraphs = limit - paragraphsWritten
+
+			if requiredParagraphs <= 0 || paragraphsWritten >= limit {
+				return
+			}
+
 			var blob, blobErr = cluster.BlobAt(blobPosition)
 
 			if blobErr != nil {
 				break
-			}
-
-			var requiredParagraphs = limit - paragraphsWritten
-
-			if requiredParagraphs <= 0 || paragraphsWritten >= limit {
-				fmt.Print("\r100.0%")
-				bufWriter.Flush()
-				txtFile.Close()
-				return
 			}
 
 			if bytes.Index(blob, []byte("<html")) >= 0 && bytes.Index(blob, []byte("</html>")) > 5 {
